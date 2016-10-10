d95f197f80b6bd488d32e6720d66aa956324aae9
MartenFriman <Twism08@hotmail.com>
2016-10-10 03:16:41 +0200

    Visual vibration + optimization

--- 4Strings/public/script.js
+++ 4Strings/public/script.js
@@ -1,7 +1,7 @@
 // enable vibration support
 navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
-document.body.addEventListener('touchmove', function(event) {
+canvas.addEventListener('touchmove', function(event) {
   event.preventDefault();
 }, false);
 
@@ -13,17 +13,25 @@ var strings3x = (canvas.width/5)*3;
 var strings4x = (canvas.width/5)*4;
 var stringsx = [strings1x, strings2x, strings3x, strings4x];
 
-var stringsStrummed = [false, false, false, false];
+var still = 0;
+var strummed = 1;
+var vibrating = 2;
+var stringsStrummed = [still, still, still, still];
+var stringVibration = [0, 0, 0, 0];
+var vibrationX = [0, 0, 0, 0];
 
 var currentString = 5;
 var touch;
 
-var obj = document.getElementById('canvas');
+var loopTimer;
+
+var obj = document.getElementById('canvas');  //Load canvas and prepare for drawing
 var ctx=obj.getContext("2d");
 
-for(i=0; i < 4; i++) {
+for(i=0; i < 4; i++) {      //Draw still strings at start
   console.log(stringsx[1]);
   ctx.beginPath();
+  ctx.lineWidth = 14-(i*2);
   ctx.moveTo(stringsx[i],0);
   ctx.lineTo(stringsx[i], 600);
   ctx.stroke();
@@ -33,11 +41,73 @@ if (navigator.vibrate) {
 	// vibration API supported
 }
 
-function vibrateMe() {
-	navigator.vibrate(10000);
-	console.log("Should vibrate!")
+//MANAGES VISUAL VIBRATION OF GUITAR STRINGS
+function handleStrings() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);    //Clear canvas
+  for (i = 0; i < 4; i++) {                            //iterate through strings
+    if(xPos > stringsx[i]-20 && xPos < stringsx[i]+20) {  //Check if touching string
+      stringsStrummed[i] = strummed;                          //Change state of string to strummed
+    }
+    if(stringsStrummed[i] === strummed && xPos > stringsx[i]-160 && xPos < stringsx[i]+160) { //Constrain length string can be drawn
+      navigator.vibrate(100000);
+      currentString = i;
+      console.log("HIT")
+      ctx.beginPath();
+      ctx.lineWidth = 14-(i*2);
+      ctx.moveTo(stringsx[i],0);
+      ctx.lineTo(xPos, yPos);
+      ctx.lineTo(stringsx[i],canvas.height);
+      ctx.stroke();
+    } else if (stringsStrummed[i] === strummed) {
+      setupVibration(i);
+      navigator.vibrate(0);
+    }
+     if (stringsStrummed[i] === vibrating) {           //Check if string is supposed to vibrate
+       ctx.beginPath();                                //Draw vibrating string
+       ctx.lineWidth = 14-(i*2);
+       ctx.moveTo(stringsx[i],0);
+       ctx.lineTo(vibrationX[i], canvas.height/2);
+       ctx.lineTo(stringsx[i],canvas.height);
+       ctx.stroke();
+       if (stringVibration[i] < 1) {                   //Determine if string should stop vibrating
+         console.log("STOPPED");
+         stringVibration[i] = 0;                       //Reset variables
+         stringsStrummed[i] = still;
+         ctx.beginPath();                              //Draw still string (temporary)
+         ctx.lineWidth = 14-(i*2);
+         ctx.moveTo(stringsx[i], 0);
+         ctx.lineTo(stringsx[i],canvas.height);
+         ctx.stroke();
+       }
+       if (vibrationX[i] < stringsx[i]) {                  //These check at which point of the vibration the string is, and reverses it
+         stringVibration[i] *=0.97;                        //Speed at which vibration diminishes
+         vibrationX[i] = stringsx[i]+stringVibration[i];   //Reverse string
+       }else if (vibrationX[i] > stringsx[i]) {
+         stringVibration[i] *=0.97;
+         vibrationX[i] = stringsx[i]-stringVibration[i];
+       }
+     } else if (stringsStrummed[i] != strummed){
+       ctx.beginPath();                              //Draw still string for non-vibrating strings
+       ctx.lineWidth = 14-(i*2);
+       ctx.moveTo(stringsx[i], 0);
+       ctx.lineTo(stringsx[i],canvas.height);
+       ctx.stroke();
+     }
+  }
 }
 
+loopTimer = setInterval(handleStrings, 16);
+
+
+function setupVibration(inputString) {
+  stringsStrummed[inputString] = vibrating;
+  vibrationX[inputString] = xPos;
+  if (xPos < stringsx[inputString]) {
+    stringVibration[inputString] = stringsx[inputString] - xPos;
+  } else {
+    stringVibration[inputString] = xPos - stringsx[inputString];
+  }
+}
 
 obj.addEventListener('touchmove', function(event) {
   // If there's exactly one finger inside this element
@@ -46,43 +116,17 @@ obj.addEventListener('touchmove', function(event) {
     // Place element where the finger is
     obj.style.left = touch.pageX + 'px';
     obj.style.top = touch.pageY + 'px';
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for(i = 0; i < 4; i++) {
-      if(touch.pageX > stringsx[i]-20 && touch.pageX < stringsx[i]+20) {  //Check if touching string
-        stringsStrummed[i] = true;
-      }
-      if(stringsStrummed[i] === true && touch.pageX > stringsx[i]-220 && touch.pageX < stringsx[i]+220) {
-        navigator.vibrate(1000);
-        currentString = i;
-        console.log("HIT")
-        ctx.beginPath();
-        ctx.moveTo(stringsx[i],0);
-        ctx.lineTo(touch.pageX, touch.pageY);
-        ctx.lineTo(stringsx[i],canvas.height);
-        ctx.stroke();
-      } else {
-        stringsStrummed[i] = false;
-        //navigator.vibrate(0);
-        ctx.beginPath();
-        ctx.moveTo(stringsx[i], 0);
-        ctx.lineTo(stringsx[i],canvas.height);
-        ctx.stroke();
-      }
-    }
+    xPos = touch.pageX;
+    yPos = touch.pageY;
   }
 }, false);
 
 obj.addEventListener('touchend', function(event) {
   if (event.changedTouches.length == 1) {
-    console.log("STOP");
-    stringsStrummed[currentString] = false;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for(i = 0; i < 4; i++) {
-      navigator.vibrate(0);
-      ctx.beginPath();
-      ctx.moveTo(stringsx[i], 0);
-      ctx.lineTo(stringsx[i],canvas.height);
-      ctx.stroke();
+    console.log("TOUCH ENDED");
+    navigator.vibrate(0);
+    if (stringsStrummed[currentString] != vibrating) {
+      setupVibration(currentString);
     }
   }
 }, false);
